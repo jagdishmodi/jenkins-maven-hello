51d89cfdc48095bc5e06e38ece1da4cd76ea9bde
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2026-03-24 20:13:15 +0530

    Update greeting message in HelloWorld class

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello,my team Jenkins + Maven!");
+        System.out.println("Hello,my team Jenkins + Maven!,new hange");
     }
 }
